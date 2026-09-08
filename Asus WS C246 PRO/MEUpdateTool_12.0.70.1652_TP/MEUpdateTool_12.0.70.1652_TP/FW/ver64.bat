c7c21ccb74e5add7c801f31f9db75c135844345b
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-09-08 18:56:17 -0400

    Add files

---
+++ Asus WS C246 PRO/MEUpdateTool_12.0.70.1652_TP/MEUpdateTool_12.0.70.1652_TP/FW/ver64.bat
@@ -0,0 +1,4 @@
+del version_fw
+del version_file
+.\FWUpdLcl64.exe -FWVER >> version_fw
+.\FWUpdLcl64.exe -FWVER ME.bin >> version_file
---
+++ Asus WS C246 PRO/MEUpdateTool_12.1.1.1022v5_T/FW/ver.bat
@@ -0,0 +1,4 @@
+del version_fw
+del version_file
+.\FWUpdLcl.exe -FWVER >> version_fw
+.\FWUpdLcl.exe -FWVER ME.bin >> version_file

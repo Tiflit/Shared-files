0fa892e1f2f63244b0d5f6163a39cdf35d625b2c
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-09-16 20:20:32 -0400

    Delete Asus WS C246 PRO directory

--- Asus WS C246 PRO/MEUpdateTool_12.0.70.1652_TP/MEUpdateTool_12.0.70.1652_TP/FW/ver.bat
+++
@@ -1,4 +0,0 @@
-del version_fw
-del version_file
-.\FWUpdLcl.exe -FWVER >> version_fw
-.\FWUpdLcl.exe -FWVER ME.bin >> version_file
--- Asus WS C246 PRO/MEUpdateTool_12.1.1.1022v5_T/FW/ver64.bat
+++
@@ -1,4 +0,0 @@
-del version_fw
-del version_file
-.\FWUpdLcl64.exe -FWVER >> version_fw
-.\FWUpdLcl64.exe -FWVER ME.bin >> version_file
